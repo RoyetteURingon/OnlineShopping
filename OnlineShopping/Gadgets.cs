33f0bf62df15a9319837a55c059c2320d751dbcc
RoyetteURingon <81809097+RoyetteURingon@users.noreply.github.com>
2021-06-09 20:46:00 +0800

    Add files via upload
    Responsibilities and Relationship

---
+++ OnlineShopping/Gadgets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShopping
+{
+    class Gadgets
+    {
+        // eto po ang gadgets section  na kung saan maviview ng user ang available items
+        public void getGadgets()
+        {
+            Console.WriteLine("Gadgets: ");
+            Console.WriteLine("Sony PS4 Controller V2 DualShock 4 Wireless Controller Original Brand New for PlayStation 4 : $64.12");
+            Console.WriteLine("Replacement PS4 Wireless Controller for PlayStation 4 : $16.11");
+            Console.WriteLine("PS4 DualShock 4 Wireless Controller Working on PS4, PC Windows 7, 8, and 10 : $14.21");
+            Console.WriteLine("");
+
+        }
+    }
+}
---
+++ OnlineShopping/login.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShopping
+{
+    class login
+    {
+        //dito naman po yung login ko na kung saan maglalog in ang user
+        public void DisplayElements()
+        {
+            shopping itemcall = new shopping();
+
+            string u, p;
+            string user = "username";
+            string pass = "password";
+            string address = "Binan Laguna";
+
+
+            Console.WriteLine("******************************");
+            Console.WriteLine("Please Login");
+            Console.Write("Enter Your Username: ");
+            u = Console.ReadLine();
+            Console.Write("Enter Your Password: ");
+            p = Console.ReadLine();
+            Console.Write("Enter Your Address:" + address);
+            Console.WriteLine("");
+
+            if (u == user && p == pass)
+            {
+                itemcall.Shopping();
+            }
+
+            else
+            {
+                Console.WriteLine("Wrong Password");
+            }
+        }        
+    }
+}
